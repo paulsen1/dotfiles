aba40c20815ae537c38faa9f6fccb852533263dc
Lachlan Paulsen <lachpaulsen@gmail.com>
2023-08-03 20:46:51 +1000

    Update set.lua with the objectively correct config
    Also add some comments

--- nvim/lua/lachpaulsen/set.lua
+++ nvim/lua/lachpaulsen/set.lua
@@ -1,9 +1,16 @@
+-- Set leader key to space.
 vim.g.mapleader = " "
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+-- Set tabs instead of spaces and tab width to 4.
+vim.opt.autoindent = true
 vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth=4
 vim.opt.smartindent = true
-vim.opt.wrap = false
+-- Set relative line numbers.
+vim.opt.relativenumber = true
+-- Set current line to show line number.
+vim.opt.nu = true
+-- Wrap lines if they roll off the screen.
+vim.opt.wrap = true
+-- Leave lines above and below when scrolling.
 vim.opt.scrolloff = 8
