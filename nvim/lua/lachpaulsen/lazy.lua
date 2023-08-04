f729372e5485c09c2f9ca752a164a8ebf945a615
Lachlan Paulsen <lachpaulsen@gmail.com>
2023-08-04 18:34:15 +1000

    Started trying to configure completion
    Mostly just copying from nvchad but will do some of my own fucking around before closing the book here
    Will need to find a new method for copilot I think.

--- nvim/lua/lachpaulsen/lazy.lua
+++ nvim/lua/lachpaulsen/lazy.lua
@@ -19,14 +19,14 @@ local plugins = {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        config = function ()
+        config = function()
             vim.cmd [[colorscheme catppuccin-mocha]]
         end
     },
     -- LSP Things
     {
         "neovim/nvim-lspconfig",
-        lazy=false,
+        lazy = false,
         config = function()
             require("lachpaulsen.plugins.lsp")
         end,
@@ -34,16 +34,59 @@ local plugins = {
     },
     {
         "williamboman/mason.nvim",
-        lazy=false,
+        lazy = false,
         dependencies = {
             "neovim/nvim-lspconfig",
         },
-        config = function ()
+        config = function()
             require("mason").setup()
         end
     },
     {
         "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            {
+                -- snippet plugin
+                "L3MON4D3/LuaSnip",
+                dependencies = "rafamadriz/friendly-snippets",
+                opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+                config = function(_, opts)
+                    require("plugins.configs.others").luasnip(opts)
+                end,
+            },
+
+            -- autopairing of (){}[] etc
+            {
+                "windwp/nvim-autopairs",
+                opts = {
+                    fast_wrap = {},
+                    disable_filetype = { "TelescopePrompt", "vim" },
+                },
+                config = function(_, opts)
+                    require("nvim-autopairs").setup(opts)
+
+                    -- setup cmp for autopairs
+                    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+                    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+                end,
+            },
+
+            -- cmp sources plugins
+            {
+                "saadparwaiz1/cmp_luasnip",
+                "hrsh7th/cmp-nvim-lua",
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-buffer",
+                "hrsh7th/cmp-path",
+            },
+        },
+        opts = function()
+            return require("plugins.configs.cmp")
+        end,
+        config = function(_, opts)
+            require("cmp").setup(opts)
+        end,
     },
     {
         "NvChad/nvterm",
