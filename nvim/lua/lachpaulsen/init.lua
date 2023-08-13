9d125f44f61cba65ae1fbe73e32122f7c0b4b581
lachlanpaulsen1 <lachlan.paulsen@universalstore.com.au>
2023-08-13 21:37:47 +1000

    Yank and Put from system clipboard.

--- nvim/lua/lachpaulsen/init.lua
+++ nvim/lua/lachpaulsen/init.lua
@@ -1,3 +1,4 @@
 require("lachpaulsen.set")
 require("lachpaulsen.lazy")
 require("lachpaulsen.mappings.windows")
+require("lachpaulsen.mappings.yank")
---
+++ nvim/lua/lachpaulsen/mappings/yank.lua
@@ -0,0 +1,5 @@
+vim.keymap.set('n', 'y', '"*y', { noremap = true })
+vim.keymap.set('n', 'p', '"*p', { noremap = true })
+
+vim.keymap.set('v', 'y', '"*y', { noremap = true })
+vim.keymap.set('v', 'p', '"*p', { noremap = true })
