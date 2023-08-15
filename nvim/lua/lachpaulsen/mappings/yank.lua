001d93c54fc7cd59f074d61d28c8608c3086da19
lachlanpaulsen1 <lachlan.paulsen@universalstore.com.au>
2023-08-16 09:46:46 +1000

    Delete from system clipboard

--- nvim/lua/lachpaulsen/mappings/yank.lua
+++ nvim/lua/lachpaulsen/mappings/yank.lua
@@ -1,5 +1,7 @@
 vim.keymap.set('n', 'y', '"*y', { noremap = true })
+vim.keymap.set('n', 'd', '"*d', { noremap = true })
 vim.keymap.set('n', 'p', '"*p', { noremap = true })
 
 vim.keymap.set('v', 'y', '"*y', { noremap = true })
 vim.keymap.set('v', 'p', '"*p', { noremap = true })
+vim.keymap.set('v', 'd', '"*d', { noremap = true })
